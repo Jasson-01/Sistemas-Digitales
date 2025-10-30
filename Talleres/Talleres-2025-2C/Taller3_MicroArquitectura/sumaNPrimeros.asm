fde0f9a2882277b407578ac35687ca9aa886a55e
Jasson-01 <jason.davila001@gmail.com>
2025-10-30 11:08:39 -0300

    Checkpoint-6
    instrucción NEG

--- Talleres/Talleres-2025-2C/Taller3_MicroArquitectura/sumaNPrimeros.asm
+++ Talleres/Talleres-2025-2C/Taller3_MicroArquitectura/sumaNPrimeros.asm
@@ -7,7 +7,7 @@ MOV R1, R0   ;Muevo el numero N al acumulador
 siguiente:
 DEC R0        ;Resto N -> N-1
 ADD R1, R0    ;sumo N-1 en el acumulador(R1) -> N + (N-1)
-CMP R0, R2    ;Comparo se N es igual a Cero
+CMP R0, R2    ;Comparo de nuevo si N es igual a Cero
 JZ terminar   ;Si R0(N) es igual a Cero(R1-CasoBase) termino la ejecución 
 JMP siguiente ;Ejecuto de nuevo el programa 
 terminar:     
